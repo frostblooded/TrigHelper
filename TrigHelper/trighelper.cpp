57fd817bcac342c95f304eb656e25ffa953295d1
Nikolay Danailov <frostblooded@yahoo.com>
2015-02-17 22:02:12 +0200

    Made a reset button

--- TrigHelper/trighelper.cpp
+++ TrigHelper/trighelper.cpp
@@ -8,6 +8,7 @@ TrigHelper::TrigHelper(QWidget *parent) :
     ui(new Ui::TrigHelper)
 {
     ui->setupUi(this);
+    TrigCalculator::Initialize();
     Display();
 }
 
@@ -18,31 +19,41 @@ TrigHelper::~TrigHelper()
 
 void TrigHelper::Display()
 {
-    ui -> a_display -> setText(QString::number(TrigCalculator::a));
-    ui -> b_display -> setText(QString::number(TrigCalculator::b));
-    ui -> c_display -> setText(QString::number(TrigCalculator::c));
-    ui -> a1_display -> setText(QString::number(TrigCalculator::a1));
-    ui -> b1_display -> setText(QString::number(TrigCalculator::b1));
-    ui -> h_display -> setText(QString::number(TrigCalculator::h));
-    ui -> alpha_display -> setText(QString::number(TrigCalculator::alpha));
-    ui -> beta_display -> setText(QString::number(TrigCalculator::beta));
+    ui->a_display->setText(QString::number(TrigCalculator::a));
+    ui->b_display->setText(QString::number(TrigCalculator::b));
+    ui->c_display->setText(QString::number(TrigCalculator::c));
+    ui->a1_display->setText(QString::number(TrigCalculator::a1));
+    ui->b1_display->setText(QString::number(TrigCalculator::b1));
+    ui->h_display->setText(QString::number(TrigCalculator::h));
+    ui->alpha_display->setText(QString::number(TrigCalculator::alpha));
+    ui->beta_display->setText(QString::number(TrigCalculator::beta));
 }
-
-
 
 void TrigHelper::on_calculate_button_clicked()
 {
-    TrigCalculator::a = ui -> a_display -> text().toFloat();
-    TrigCalculator::b = ui -> b_display -> text().toFloat();
-    TrigCalculator::c = ui -> c_display -> text().toFloat();
-    TrigCalculator::a1 = ui -> a1_display -> text().toFloat();
-    TrigCalculator::b1 = ui -> b1_display -> text().toFloat();
-    TrigCalculator::h = ui -> h_display -> text().toFloat();
-    TrigCalculator::alpha = ui -> alpha_display -> text().toFloat();
-    TrigCalculator::alpha_in_radians = TrigCalculator::alpha * M_PI / 180;
-    TrigCalculator::beta = ui -> beta_display -> text().toFloat();
-    TrigCalculator::beta_in_radians = TrigCalculator::beta * M_PI / 180;
+    if(ui->calculate_button->text() == "Calculate")
+    {
+        TrigCalculator::a = ui->a_display->text().toFloat();
+        TrigCalculator::b = ui->b_display->text().toFloat();
+        TrigCalculator::c = ui->c_display->text().toFloat();
+        TrigCalculator::a1 = ui->a1_display->text().toFloat();
+        TrigCalculator::b1 = ui->b1_display->text().toFloat();
+        TrigCalculator::h = ui->h_display->text().toFloat();
+        TrigCalculator::alpha = ui->alpha_display->text().toFloat();
+        TrigCalculator::alpha_in_radians = TrigCalculator::alpha * M_PI / 180;
+        TrigCalculator::beta = ui->beta_display->text().toFloat();
+        TrigCalculator::beta_in_radians = TrigCalculator::beta * M_PI / 180;
 
-    TrigCalculator::Calculate();
+        TrigCalculator::Calculate();
+
+        ui->calculate_button->setText("Reset");
+    }
+    else
+    {
+        TrigCalculator::Initialize();
+
+        ui->calculate_button->setText("Calculate");
+    }
+
     Display();
 }
